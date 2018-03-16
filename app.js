c962cdbbb98cc82e67e56e67fa0bb98cc3b2ea81
Andrew <andrew.vinocur@gmail.com>
2018-03-16 14:08:56 -0700

    fixes board model and builds turn counter fn

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const boardArr = [
-  [0],[0],[0],
-  [0],[0],[0],
-  [0],[0],[0]
+  0,0,0,
+  0,0,0,
+  0,0,0
 ];
+
+const turnCount = 0;
+
+function turnCounter() {
+  turnCount++;
+  if(turnCount % 2 == 0) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
+}
 
 $(document).ready(function(){
 
